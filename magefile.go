ddad1cd8370ae571aff5aa3e380894d429738ca2
Jeremy Fergason <jeremy.fergason@corwynn.com>
2022-05-26 13:48:53 -0700

    Revert "update copyright year to 2022"
    This reverts commit 2e9c0514dbce56b18724abeddf318092c00b2d87.

--- magefile.go
+++ magefile.go
@@ -1,6 +1,6 @@
 //go:build mage
 
-// Copyright 2022
+// Copyright 2021
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
